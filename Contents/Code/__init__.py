29e6a5d33297b917774bcb96f31294d962780faf
Tommy Mikkelsen <tm@orstrand.dk>
2014-12-28 14:23:45 +0100

    Added some fixed for routes and prefs

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -24,6 +24,7 @@ ICON = 'icon-WebChannel.png'
 PREFIX = '/applications/WebChannels'
 APPGUID = '7608cf36-742b-11e4-8b39-00089be320f4'
 DESCRIPTION = 'Acts as a webserver for other channels/bundles'
+ROUTES = {}
 
 ####################################################################################################
 # Start function
@@ -31,13 +32,25 @@ DESCRIPTION = 'Acts as a webserver for other channels/bundles'
 def Start():
 	print("********  Started %s on %s  **********" %(NAME  + VERSION, Platform.OS))
 	Log.Debug("*******  Started %s on %s  ***********" %(NAME  + VERSION, Platform.OS))
+	global myWeb
 	Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
 	ObjectContainer.art = R(ART)
 	ObjectContainer.title1 = NAME  + VERSION
 	ObjectContainer.view_group = 'List'
 	DirectoryObject.thumb = R(ICON)
 	HTTP.CacheTime = 0
-	StartWeb()
+	GetRoutes()
+	# Start WebServer
+	myWeb = WebServer()
+	myWeb.start()
+
+####################################################################################################
+# Get Routes to valid document roots
+####################################################################################################
+@route(PREFIX + '/GetRoutes')
+def GetRoutes():
+	global ROUTES	
+	ROUTES = ast.literal_eval('{' + Prefs['Routes'] + '}')
 
 ####################################################################################################
 # Main menu
@@ -57,55 +70,76 @@ def MainMenu():
 	Log.Debug("**********  Ending MainMenu  **********")
 	return oc
 
-####################################################################################################
-# Start WebServer
-####################################################################################################
-@route(PREFIX + '/StartWeb')
-def StartWeb():
-	# Get document roots from Prefs
-	ROUTES = ast.literal_eval('{' + Prefs['Routes'] + '}')
+#***********************************************************************
+# ValidatePrefs
+#***********************************************************************
+@route(PREFIX + '/ValidatePrefs')
+def ValidatePrefs():
+	Log.Debug('Validating Prefs')
+	GetRoutes()
+	myWeb.restart()
 
-	class RequestHandler(SimpleHTTPRequestHandler):
-		# Request Handler for bundle paths
-		def translate_path(self, path):
-			"""translate path given routes"""
+#***********************************************************************
+# WebServer
+#***********************************************************************
+class WebServer(HTTPServer):
+	def __init__(self):
+		self.server_address = '0.0.0.0'
+		self.HandlerClass = RequestHandler
+		Log.Debug('* http deamon init okay')
 
-			# set default root to cwd
-			root = os.path.join(Core.app_support_path, 'Plug-ins', 'WebChannel.bundle', 'http-root')
-		      
-			# look up routes and set root directory accordingly
-			for pattern, rootdir in ROUTES.items():
-				if path.startswith(pattern):
-					# found match!
-					path = path[len(pattern):]  # consume path up to pattern len
-					root = rootdir
-					break
-		      
-			# normalize path and prepend root directory
-			path = path.split('?',1)[0]
-			path = path.split('#',1)[0]
-			path = posixpath.normpath(urllib.unquote(path))
-			words = path.split('/')
-			words = filter(None, words)				  
-			path = root
-			for word in words:
-				drive, word = os.path.splitdrive(word)
-				head, word = os.path.split(word)
-				if word in (os.curdir, os.pardir):
-					continue
-				path = os.path.join(path, word)
-			return path
+	def start(self):
+		Log.Debug('* Starting httpd deamon')
+		self.server_port = int(Prefs['Port'])
+		self.server = HTTPServer((self.server_address, self.server_port), self.HandlerClass)	
+		thread = threading.Thread(target = self.server.serve_forever)
+		thread.deamon = True
+		thread.start()
+		sa = self.server.socket.getsockname()
+		print "* Serving HTTP on", sa[0], "port", sa[1], "..."
+		Log.Debug('* Serving HTTP on %s port %s ....' %(sa[0],sa[1]))
 
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', int(Prefs['Port'])), RequestHandler)
+	def stop(self):
+		print 'Stopping httpd deamon.....Please wait'
+		Log.Debug('Shutting down httpd deamon')
+		self.server.shutdown()
 
-	Log.Debug('Started http server on port %s' %(Prefs['Port']))	
-	Log.Debug('Serving the following: %s' %(ROUTES))
-	
-	#Wait forever for incoming htto requests
-	thread = threading.Thread(target = server.serve_forever)
-	thread.deamon = True
-	thread.start()
-	return
+	def restart(self):
+		Log.Debug('Restarting WebServer')
+		self.stop()
+		self.start()
+
+#***********************************************************************
+# RequestHandler
+#***********************************************************************
+class RequestHandler(SimpleHTTPRequestHandler):
+	# Request Handler for bundle paths
+	def translate_path(self, path):
+		"""translate path given routes"""
+
+		# set default root to this bundles http directory
+		root = os.path.join(Core.app_support_path, 'Plug-ins', 'WebChannel.bundle', 'http-root')
+	      
+		# look up routes and set root directory accordingly to the request
+		for pattern, rootdir in ROUTES.items():
+			if path.startswith(pattern):
+				# found match!
+				path = path[len(pattern):]  # consume path up to pattern len
+				root = rootdir
+				break
+	      
+		# normalize path and prepend root directory
+		path = path.split('?',1)[0]
+		path = path.split('#',1)[0]
+		path = posixpath.normpath(urllib.unquote(path))
+		words = path.split('/')
+		words = filter(None, words)				  
+		path = root
+		for word in words:
+			drive, word = os.path.splitdrive(word)
+			head, word = os.path.split(word)
+			if word in (os.curdir, os.pardir):
+				continue
+			path = os.path.join(path, word)
+		return path
 
